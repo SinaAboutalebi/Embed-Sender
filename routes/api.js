f9c8d8b0ddefa921df4e08c17c2882e5e94d9228
Zer0Power <zeroopoower@gmail.com>
2022-11-16 20:16:37 +0330

    feat: view Router  &

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,6 @@
 const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
-const { EmbedBuilder } = require("discord.js");
 
 //---------------------------💔🚬 'Zer0Power 💔🚬---------------------------//
 //Routes
---
+++ routes/views.js
@@ -0,0 +1,91 @@
+//---------------------------💔🚬 'Zer0Power 💔🚬---------------------------//
+//Packages
+
+const express = require("express");
+const router = express.Router();
+const fetch = require("node-fetch");
+
+//---------------------------💔🚬 'Zer0Power 💔🚬---------------------------//
+//Routes
+
+//Home View Router==========================================================//
+
+router.get("/", async (req, res) => {
+  let token = req.cookies["token"];
+
+  if (!token) {
+    return res.render("login", { data: "none" });
+  }
+
+  if (token != process.env.SECRET_KEY) {
+    res.clearCookie("token");
+
+    return res.render("login", { data: "none" });
+  }
+
+  let data = await fetch("https://discordapp.com/api/v9/users/@me/guilds", {
+    method: "GET",
+    headers: {
+      Authorization: `Bot ${process.env.TOKEN}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      return json;
+    });
+
+  res.render("servers", { data: data });
+});
+
+//Server View Router========================================================//
+
+router.get("/server/:id", async (req, res) => {
+  let token = req.cookies["token"];
+
+  if (!token) {
+    return res.render("login", { data: "none" });
+  }
+
+  if (token != process.env.SECRET_KEY) {
+    res.clearCookie("token");
+
+    return res.render("login", { data: "none" });
+  }
+
+  let { id } = req.params;
+
+  let data = await fetch(
+    `https://discordapp.com/api/v9/guilds/${id}/channels`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bot ${process.env.TOKEN}`,
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      return json;
+    });
+
+  let roles = await fetch(`https://discordapp.com/api/v9/guilds/${id}/roles`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bot ${process.env.TOKEN}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      return json;
+    });
+
+  if (!data[0]) {
+    res.render("404");
+  } else {
+    res.render("embed", { data: data, roles: roles });
+  }
+});
+//---------------------------💔🚬 'Zer0Power 💔🚬---------------------------//
+//Exports Router
+
+module.exports = router;
