647e81ae687c22db97edc4df7f08f930d872ef9c
Saman-Safaei <xsamansafaeix@gmail.com>
2022-12-02 20:20:01 +0330

    add custom config for box shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./views/**/*.ejs"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "0px 30px 60px -5px #000",
+        "custom-button": "0px 4px 35px -5px #4082f5",
+      },
+    },
   },
   plugins: [],
-}
+};
